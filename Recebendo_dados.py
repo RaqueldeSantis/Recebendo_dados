39708dcb4df452df0c5f99416205d9c0193b0d73
RaqueldeSantis <raqueldesantis93@gmail.com>
2024-11-04 19:54:33 -0300

    Add files via upload

---
+++ Recebendo_dados.py
@@ -0,0 +1,10 @@
+print('Qual é o seu nome?')
+nome = input( )
+
+print(f' Seja Bem Vindo(a) {nome}')
+print('Qual é a sua idade? ')
+idade = input( )
+
+print(f' {nome} tem {idade} anos')
+print(f' {nome} nasceu em {2024 - int (idade)}')
+
